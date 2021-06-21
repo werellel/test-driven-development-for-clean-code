6f420305989ed022ff06bf9c7643111c2e3b328a
werellel <werellel@gmail.com>
2021-06-21 23:42:07 +0900

    Add unittest example

--- functional_tests.py
+++ functional_tests.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
+import unittest
 
-# 에디스는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
-# 해당 웹 사이트를 확인하러 간다.
-browser.get('http://localhost:8000')
-
-# 웹 페이지 타이틀과 헤더가 'To-DO'를 표시하고 있다.
-assert 'To-Do' in browser.title
-
-# 바로 작업을 추가하기로 한다.
-
-# 공작깃털 사기라고 텍스트 상자에 입력
-
-# 엔터키를 치면 페이지가 갱신되고 작업 목록에 아이템이 추가된다.
-
-#...
-
-# 만족하고 잠에 든다.
-browser.quit()
+class NewVisitorTest(unittest.TestCase): # !
+    def setUp(self): # 2
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self): # 3
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self): # 4
+        # 에디스는 작업 목록 오라인 앱이 나왔다는 소식을 듣고 웹 사이트를 확인하러 간다.
+        self.browser.get('http://localhost:8888')
+        
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
+        self.assertIn("To-Do", self.browser.title) # 5
+        self.fail("Finish the test!") # 6
+        
+        # 그녀는 바로 작업을 추가하기로 한다.
+if __name__ == "__main__": # 7
+    unittest.main(warnings='ignore') # 8
