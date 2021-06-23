a21732fc4e4cfdefbfe5299cae8f2253f81db978
werellel <werellel@gmail.com>
2021-06-23 23:46:04 +0900

    Add unittest in django

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SmokeTest(TestCase):
+	def test_bad_maths(self):
+		self.assertEqual(1 + 1, 3)
+
+
+# from django.core.urlresolvers import resolve
+from django.urls import reverse
+from lists.views import home_page # 1
+
+class HomePageTest(TestCase):
+	def test_root_url_resolves_to_home_page_view(self):
+		found = reverse('/') # 2
+		self.assertEqual(found.func, home_page) # 3
