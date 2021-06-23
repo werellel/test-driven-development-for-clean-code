b03a5d33caf94a7bc793c03500e166395625e8cc
werellel <werellel@gmail.com>
2021-06-23 23:59:18 +0900

    Add view of home_page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
-class SmokeTest(TestCase):
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+# 	def test_bad_maths(self):
+# 		self.assertEqual(1 + 1, 3)
 
 
 # from django.core.urlresolvers import resolve
