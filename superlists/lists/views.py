578a1a44e1878bcc62f8c035c31db5350e817ce7
werellel <werellel@gmail.com>
2021-06-26 23:54:15 +0900

    Add httpResponse to home_page

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import httpResponse
 
 # Create your views here.
 
 def home_page():
-	pass
+	return httpResponse('<html><title>To-Do lists</title>')
